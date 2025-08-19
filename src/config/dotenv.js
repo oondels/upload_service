7b9e47d45a01db37cd4e13d80ee61010066a6935
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-08-19 10:33:51 -0300

    fix: remove FINAL_UPLOAD_FOLDER and set default UPLOAD_FOLDER value in dotenv configuration

--- src/config/dotenv.js
+++ src/config/dotenv.js
@@ -6,8 +6,8 @@ require("dotenv").config({
 
 const vars = {
   RABBITMQ_URL: process.env.RABBITMQ_URL,
-  UPLOAD_FOLDER: process.env.UPLOAD_FOLDER || "/app/uploads",
-  FINAL_UPLOAD_FOLDER: process.env.UPLOAD_FINAL_FOLDER || "/var/www/dasssest.com/uploads",
+  UPLOAD_FOLDER: process.env.UPLOAD_FOLDER || "",
+  FILE_URL_PATH: process.env.FILE_URL_PATH || "http://localhost/uploads/",
   MAX_FILE_SIZE: process.env.MAX_FILE_SIZE,
   ALLOWED_FILE_TYPES: process.env.ALLOWED_FILE_TYPES,
   DEV_ENV: process.env.DEV_ENV,
