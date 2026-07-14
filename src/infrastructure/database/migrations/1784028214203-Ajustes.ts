ce39da5c7be88e21ea19b3e168b69b90efc9977e
Hendrius Dass <hendriusfelix.dev@gmail.com>
2026-07-14 08:30:55 -0300

    feat(migrations): add updated_at column to applications table

---
+++ src/infrastructure/database/migrations/1784028214203-Ajustes.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Ajustes1784028214203 implements MigrationInterface {
+    name = 'Ajustes1784028214203'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."applications" ADD "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."applications" DROP COLUMN "updated_at"`);
+    }
+
+}
